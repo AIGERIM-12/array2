93e5faffba082332455f358434c4e3c78a9173c7
AIGERIM-12 <aigerim.aibekova@alatoo.edu.kg>
2024-11-29 11:30:23 +0600

    Update array2.py

--- array2.py
+++ array2.py
@@ -1,114 +1,79 @@
 import os
-
-# Очистка экрана
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Словарь для преобразования букв в индексы столбцов
-letters_to_index = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
+clear_screen()
+print("Welcome to the 'Battleship' game!")
 
-# Функция для запуска одной игры
-def play_game():
-    # Игровое поле 7x7
+results = []
+
+while True:
+    name = input("What's your name, player? ")
+
     board = [['~' for _ in range(7)] for _ in range(7)]
-
-    # Ставим корабль из 3 клеток (горизонтально) в строке 2, начиная с колонки 2
-    ship = [(1, 1), (1, 2), (1, 3)]  # Корабль состоит из 3 клеток
-
-    # Счётчик выстрелов
+    ship = [(1, 1), (1, 2), (1, 3)]
     shots = 0
-    shot_positions = set()  # Множество для хранения выстреленных позиций
-
-    # Приветствие
-    print("Добро пожаловать в игру 'Морской бой'!")
-    name = input("Как вас зовут, игрок? ")
 
     while True:
         clear_screen()
 
-        # Печатаем игровое поле
         print("   A B C D E F G")
         print("  ---------------")
         for row in range(7):
-            print(f"{row+1} | {' '.join(board[row])}")
+            print(f"{row + 1} | {' '.join(board[row])}")
 
-        print(f"Выстрелов: {shots}")
+        print(f"Shots: {shots}")
 
-        # Ввод координат выстрела
-        shot = input("Введите координаты для выстрела (например, A1): ").strip().upper()
+        shot = input("Enter coordinates for your shot (e.g., A1): ").strip().upper()
 
-        # Проверка на правильность ввода
-        if len(shot) != 2 or shot[0] not in letters_to_index or not shot[1].isdigit():
-            print("Неверный формат ввода. Попробуйте снова!")
-            continue
-        
-        col = letters_to_index[shot[0]]  # Преобразуем букву в индекс столбца
-        row = int(shot[1]) - 1  # Преобразуем цифру в индекс строки
-
-        # Проверка на правильность координат (в пределах поля)
-        if not (0 <= row <= 6 and 0 <= col <= 6):
-            print("Координаты вне поля. Попробуйте снова!")
+        if len(shot) != 2 or shot[0] not in 'ABCDEFG' or not shot[1].isdigit():
+            print("Invalid format. Try again!")
+            input("Press Enter to continue...")
             continue
 
-        # Проверка, был ли уже выстрел в эту клетку
-        if (row, col) in shot_positions:
-            print("Вы уже стреляли в эту клетку. Попробуйте снова!")
+        col = 'ABCDEFG'.index(shot[0])
+        row = int(shot[1]) - 1
+
+        if row < 0 or row > 6 or col < 0 or col > 6:
+            print("Coordinates out of bounds. Try again!")
+            input("Press Enter to continue...")
             continue
 
-        # Увеличиваем счётчик выстрелов
+        if board[row][col] != '~':
+            print("You already shot here. Try another cell!")
+            input("Press Enter to continue...")
+            continue
+
         shots += 1
-        shot_positions.add((row, col))  # Добавляем позицию в множество выстреленных клеток
 
-        # Проверка попадания
         if (row, col) in ship:
-            print("Попадание!")
-            board[row][col] = 'X'  # Отметим попадание
-            ship.remove((row, col))  # Удалим часть корабля
+            print("Hit!")
+            board[row][col] = 'H'
+            ship.remove((row, col))
+            input("Press Enter to continue...")
         else:
-            print("Промах!")
-            board[row][col] = '*'  # Отметим промах
+            print("Miss!")
+            board[row][col] = 'M'
+            input("Press Enter to continue...")
 
-        # Проверка на победу (если все части корабля потоплены)
         if len(ship) == 0:
-            clear_screen()
-            print("   A B C D E F G")
-            print("  ---------------")
-            for row in range(7):
-                print(f"{row+1} | {' '.join(board[row])}")
-            
-            print(f"Поздравляем, {name}! Вы потопили корабль за {shots} выстрелов.")
-            return name, shots  # Возвращаем имя игрока и количество выстрелов
+            print(f"Congratulations, {name}! You sank the ship in {shots} shots!")
 
-        # Ожидаем нажатия Enter перед следующим ходом
-        input("Нажмите Enter, чтобы продолжить...")
+            results.append((name, shots))
 
-# Главная программа
-def main():
-    players_scores = []  # Список для хранения результатов игроков
+            play_again = input("Do you want to play again? (y/n): ").strip().lower()
+            if play_again != 'y':
+                print("\nPlayer rankings:")
+                rank = 1
+                for player in results:
+                    print(f"{rank}. {player[0]} - {player[1]} shots")
+                    rank += 1
+                print("Thanks for playing!")
+                exit()
 
-    while True:
-        # Запускаем одну игру
-        name, shots = play_game()
-        
-        # Добавляем результат в список
-        players_scores.append((name, shots))
-
-        # Спрашиваем игрока, хочет ли он сыграть снова
-        play_again = input("Хотите сыграть еще раз? (y/n): ").strip().lower()
-        if play_again != 'y':
-            # Сортируем игроков по количеству выстрелов (от лучшего к худшему)
-            sorted_players = sorted(players_scores, key=lambda x: x[1])
-
-            # Выводим таблицу результатов
-            print("\nРейтинг игроков (от лучшего к худшему):")
-            for idx, (player, score) in enumerate(sorted_players, 1):
-                print(f"{idx}. {player} - {score} выстрелов")
-
-            print("\nСпасибо за игру!")
-            break  # Завершаем программу
-
-# Запуск главной программы
-if __name__ == "__main__":
-    main()
+            board = [['~' for _ in range(7)] for _ in range(7)]
+            ship = [(1, 1), (1, 2), (1, 3)]
+            shots = 0
+            continue
 
 
