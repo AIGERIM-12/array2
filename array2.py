bef84a7951c6008ddb18433ca1afd76c8cb888ac
AIGERIM-12 <aigerim.aibekova@alatoo.edu.kg>
2024-12-04 10:58:34 +0600

    Update array2.py

--- array2.py
+++ array2.py
@@ -1,103 +1,87 @@
 import os
 import random
+os.system('cls' if os.name == 'nt' else 'clear')
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+print("Welcome to sea battle!")
 
-clear_screen()
-print("Welcome to the 'Battleship' game!")
+grid = [['~' for _ in range(7)] for _ in range(7)]  
+ships = []
 
-results = []
-def place_ship():
-    ship = []
-    while len(ship) < 3:
-        
-        start_row = random.randint(0, 6)
-        start_col = random.randint(0, 6)
-      
+ship_sizes = [3, 2, 2, 1, 1, 1, 1]
+
+for size in ship_sizes:
+    placed = False
+    while not placed:
+        row = random.randint(0, 6)
+        col = random.randint(0, 6)
         orientation = random.choice(["horizontal", "vertical"])
+        
+        if orientation == "horizontal" and col + size <= 7:
+            if all(grid[row][col + i] == '~' for i in range(size)):
+                ships.append([(row, col + i) for i in range(size)])
+                placed = True
+      
+        elif orientation == "vertical" and row + size <= 7:
+            if all(grid[row + i][col] == '~' for i in range(size)):
+                ships.append([(row + i, col) for i in range(size)])
+                placed = True
+shots = 0
+name = input("What's your name, player? ")
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+ 
+    print("   A B C D E F G")
+    print("  ---------------")
+    for row in range(7):
+        print(f"{row + 1} | {' '.join(grid[row])}")
+    
+    print(f"Shots taken: {shots}")
+    
+    shot = input("Enter coordinates for your shot (e.g., A1): ").upper()
+
+    if len(shot) != 2 or shot[0] not in 'ABCDEFG' or not shot[1].isdigit():
+        print("Invalid input. Try again!")
+        input("Press Enter to continue...")
+        continue
+
+    col = 'ABCDEFG'.index(shot[0])
+    row = int(shot[1]) - 1
 
     
-        if orientation == "horizontal":
-          
-            if start_col + 2 <= 6:
-                ship = [(start_row, start_col), (start_row, start_col + 1), (start_row, start_col + 2)]
-        else:  
-          
-            if start_row + 2 <= 6:
-                ship = [(start_row, start_col), (start_row + 1, start_col), (start_row + 2, start_col)]
+    if row < 0 or row > 6 or col < 0 or col > 6:
+        print("Out of bounds. Try again!")
+        input("Press Enter to continue...")
+        continue
 
-    return ship
-
-while True:
-    name = input("What's your name, player? ")
-
-    board = [['~' for _ in range(7)] for _ in range(7)]
-    ship = place_ship()  
-    shots = 0
-
-    while True:
-        clear_screen()
-
-        print("   A B C D E F G")
-        print("  ---------------")
-        for row in range(7):
-            print(f"{row + 1} | {' '.join(board[row])}")
-
-        print(f"Shots: {shots}")
-
-        shot = input("Enter coordinates for your shot (e.g., A1): ").upper()
-
-       
-        if len(shot) != 2 or shot[0] not in 'ABCDEFG' or not shot[1].isdigit():
-            print("Invalid format. Try again!")
-            input("Press Enter to continue...")
-            continue
-
-        col = 'ABCDEFG'.index(shot[0])
-        row = int(shot[1]) - 1
-
-      
-        if row < 0 or row > 6 or col < 0 or col > 6:
-            print("Coordinates out of bounds. Try again!")
-            input("Press Enter to continue...")
-            continue
-
-        if board[row][col] != '~':
-            print("You already shot here. Try another cell!")
-            input("Press Enter to continue...")
-            continue
-
-        shots += 1
-
+    if grid[row][col] != '~':
+        print("You already shot here. Try another cell!")
+        input("Press Enter to continue...")
+        continue
+    shots += 1
+    hit = False
+    for ship in ships:
         if (row, col) in ship:
             print("Hit!")
-            board[row][col] = 'H'
+            grid[row][col] = 'H' 
             ship.remove((row, col))  
-            input("Press Enter to continue...")
-        else:
-            print("Miss!")
-            board[row][col] = 'M'
-            input("Press Enter to continue...")
+            hit = True
+            break
 
-        if len(ship) == 0:
-            print(f"Congratulations, {name}! You sank the ship in {shots} shots!")
+    if not hit:
+        print("Miss!")
+        grid[row][col] = 'M'  
 
-            results.append((name, shots))
+    input("Press Enter to continue...")
 
-            play_again = input("Do you want to play again? (yes/no): ").lower()
-            if play_again != 'yes':
-                print("\nPlayer rankings:")
-                rank = 1
-                for player in results:
-                    print(f"{rank}. {player[0]} - {player[1]} shots")
-                    rank += 1
-                print("Thanks for playing!")
-                exit()
+    
+    if all(len(ship) == 0 for ship in ships):
+        print(f"Congratulations, {name}! You sank all the ships in {shots} shots!")
+        break
 
-            board = [['~' for _ in range(7)] for _ in range(7)]
-            ship = place_ship()  
-            shots = 0
-            continue
+play_again = input("Do you want to play again? (yes/no): ").lower()
+if play_again != 'yes':
+    print("Thanks for playing!")
+
 
 
